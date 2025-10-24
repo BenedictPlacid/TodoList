138204a1280afe4633dec8baa7f2364a74f62c9a
bndt <benedictplacid101@gmail.com>
2025-10-24 12:16:52 +0530

    Initial Commit

---
+++ ToDoLIst/ToDoLIstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoLIstApp.swift
+//  ToDoLIst
+//
+//  Created by benedict on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoLIstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
